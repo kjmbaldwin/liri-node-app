681e4bcf8efbe4a2ca2b044c92e1e4ad038f615e
Kyle Baldwin <kylejmbaldwin@gmail.com>
2017-12-24 00:24:36 -0500

    Initial file setup

---
+++ keys.js
@@ -0,0 +1,10 @@
+console.log('twitter keys are loaded');
+
+var twitterKeys = {
+  consumer_key: '',
+  consumer_secret: '',
+  access_token_key: '',
+  access_token_secret: '',
+}
+
+module.exports = twitterKeys;
---
+++ liri.js
@@ -0,0 +1,4 @@
+var request = require('request');
+var spotify = require('node-spotify-api');
+var twitter = require('twitter');
+var twitterKeys = require("./keys.js");
