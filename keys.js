97a453c81f2c272a0ee19414fe7e74335cea066d
Kyle Baldwin <kylejmbaldwin@gmail.com>
2018-01-09 00:14:20 -0500

    created .env file for keys, and finished spotify functions

--- keys.js
+++ keys.js
@@ -1,10 +1,15 @@
-console.log('twitter keys are loaded');
+console.log('keys are loaded');
 
-var twitterKeys = {
-  consumer_key: '',
-  consumer_secret: '',
-  access_token_key: '',
-  access_token_secret: '',
-}
+exports.twitter = {
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+};
 
-module.exports = twitterKeys;
+exports.spotify = {
+  id: process.env.SPOTIFY_ID,
+  secret: process.env.SPOTIFY_SECRET
+};
+
+exports.omdb = process.env.OMDB_KEY
--- liri.js
+++ liri.js
@@ -1,4 +1,132 @@
+require("dotenv").config();
+var keys = require("./keys.js");
+
 var request = require('request');
-var spotify = require('node-spotify-api');
+var Spotify = require('node-spotify-api');
 var twitter = require('twitter');
-var twitterKeys = require("./keys.js");
+var fs = require('fs');
+
+var spotify = new Spotify(keys.spotify);
+var omdbKey = keys.omdb;
+var twitter = new Twitter(keys.twitter);
+
+var liriAction = process.argv[2];
+
+
+switch(liriAction){
+  case 'movie-this':
+    omdb();
+    break;
+
+  case 'my-tweets':
+    twitter();
+    break;
+
+  case 'spotify-this-song':
+    spotifySearch();
+    break;
+
+  default:
+    console.log('Sorry, I didn\'t understand that. You can try:\nmovie-this\nmy-tweets\nspotify-this-song');
+    break;
+};
+
+
+function omdb(){
+
+  //grab first element
+  var queryString = process.argv[3];
+
+  //if there are addtional words concatenate them onto the query
+  if(process.argv[4]){
+    for (var i = 4; i < process.argv.length; i++) {
+      queryString += '+' + process.argv[i];
+    }
+  }
+
+  if(!queryString){ 
+    //if the user did not provide anything, set search to Mr Nobody
+    queryString = 'Mr.+Nobody';
+    var apiURL = 'http://www.omdbapi.com/?apikey=' + omdbKey + '&t=' + queryString;
+    omdbSearch(apiURL);
+  } else { //else, run search as normal
+    var apiURL = 'http://www.omdbapi.com/?apikey=' + omdbKey + '&t=' + queryString;
+    omdbSearch(apiURL);
+  }
+
+};
+
+//run the API and format the results
+function omdbSearch(urlRef){
+
+  request(urlRef, function (error, response, body) {
+
+    if(!error && response.statusCode === 200){
+      
+      console.log('######################################################');
+      console.log('#');
+      console.log('#               ' + JSON.parse(body).Title);
+      console.log('#');
+      console.log('######################################################');
+      console.log('');
+
+      console.log('Release Year: ' + JSON.parse(body).Year);
+
+      if(JSON.parse(body).Ratings[0].Value){
+        console.log('IMDB rating: ' + JSON.parse(body).Ratings[0].Value);
+      };
+
+      if(JSON.parse(body).Ratings[1].Value){
+        console.log('Rotten Tomatoes rating: ' + JSON.parse(body).Ratings[1].Value);
+      };
+      
+      console.log('Produced in: ' +JSON.parse(body).Country);
+      console.log('Language: ' + JSON.parse(body).Language);
+      console.log('Plot: ' + JSON.parse(body).Plot);
+      console.log('Actors: ' + JSON.parse(body).Actors);
+      console.log('');
+      console.log('######################################################');
+    }
+
+    if (error || response.statusCode === 400){
+      console.log('Whoops, I had an error. Please try again.');
+    }
+
+
+  });
+};
+
+function spotifySearch(){
+
+  //grab first element
+  var spotifyString = process.argv[3];
+
+  //if there are addtional words concatenate them onto the query
+  if(process.argv[4]){
+    for (var i = 4; i < process.argv.length; i++) {
+      spotifyString += ' ' + process.argv[i];
+    }
+  }
+
+spotify.search({ type: 'track', query: spotifyString, limit: 1 }, function(err, data) {
+  if (err) {
+    return console.log('Error occurred: ' + err);
+  }
+ 
+console.log(JSON.stringify(data, null, 2));
+
+var apiReturn = data.tracks.items[0];
+var artistName = apiReturn.artists[0].name;
+var songName = apiReturn.name;
+var songURL = apiReturn.album.external_urls.spotify;
+var albumName = apiReturn.album.name;
+
+console.log("Artist Name: " + artistName + '\nSong Name: ' + songName + "\nPreview URL: " + songURL + "\nAlbum Name: " + albumName);
+
+});
+
+};
+
+function twitter(){
+
+};
